78dcbcadb45fb3035fc5847e4042629ad6a53b59
hlanza <hugo.lanza@proconsumo.hn>
2015-03-27 17:58:32 -0600

    Se agrego la clase de xmlLibrary

---
+++ framework/xml/xmlLibrary.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Xml
+Imports System.IO
+Public Class XmlDocumentEX
+    Private xml As XmlDocument
+    Sub New(xml As XmlDocument)
+        Me.xml = xml
+    End Sub
+    Public Function toDataTable() As DataTable
+        Dim ms As MemoryStream = Nothing
+        Dim returnMs As New DataTable()
+        Try
+            Dim ds As New DataSet
+            ds.ReadXml(New XmlNodeReader(xml))
+            Return ds.Tables(0)
+        Catch ex As Exception
+            Return returnMs
+        Finally
+            If Not ms Is Nothing Then
+                ms.Close()
+            End If
+        End Try
+    End Function
+End Class
+
